1ba3c46e1a0cd06e11be8ad7423559734b3e01c4
mattlourenco27 <matt.lourenco27@gmail.com>
2020-03-02 11:38:36 -0500

    Backtracking algorithm implemented. Extensive testing not complete

--- sudoku.py
+++ sudoku.py
@@ -26,9 +26,6 @@ from numpy import array
 # Returns true if the number 'value' can be played at given row and column. Returns false if the number 'value' cannot
 #   be placed the given row and column
 def valid_play(row, col, value):
-    assert isinstance(row, int)
-    assert isinstance(col, int)
-    assert isinstance(value, int)
 
     # Check for existing number
     if board[row][col] != 0:
@@ -61,8 +58,6 @@ def valid_play(row, col, value):
 # Outputs: List
 # Returns a list of valid numbers that can be placed at given row and column
 def valid_numbers(row, col):
-    assert isinstance(row, int)
-    assert isinstance(col, int)
 
     valid_nums = []
 
@@ -83,14 +78,70 @@ def valid_numbers(row, col):
 # reached that has no solutions. It then backtracks to try other numbers in previous squares until all options are
 # exhausted. If the board is valid at any point then it returns true.
 def solve_backtracking():
-    pass
+    solve_backtracking_helper(0, 0)
+    return valid_board()
+
+
+# Inputs: (int) row, (int) column
+# Outputs: Success at tile (Boolean)
+# Helper function to recursively solve the sudoku board
+def solve_backtracking_helper(row, col):
+
+    # Check that row and col did not go out of bounds
+    if row < 0 or row > 8 or col < 0 or col > 8:
+        print("Row or column out of range\nRow: " + row + "\nColumn: " + col)
+        return False
+
+    # numbers_to_play holds a list of valid numbers at this tile
+    numbers_to_play = valid_numbers(row, col)
+
+    if not numbers_to_play:
+        # There are no valid numbers
+        if board[row][col] != 0:
+            # If there was already a number there, move forward
+            if col == 8:
+                if solve_backtracking_helper(row + 1, 0):
+                    return True
+            else:
+                if solve_backtracking_helper(row, col + 1):
+                    return True
+
+            # The numbers after this did not work. Backtrack
+            return False
+
+        else:
+            # No possible numbers and empty square. Need to backtrack
+            return False
+
+    if numbers_to_play and row == 8 and col == 8:
+        # List has an option to place at this square and the game is finished
+        # There should only be one valid number to place
+        board[row][col] = numbers_to_play[0]
+        return True
+
+    for i in numbers_to_play:
+        # try all numbers
+        board[row][col] = i;
+
+        # Move on to see if the rest of the board can be solved
+        if col == 8:
+            if solve_backtracking_helper(row + 1, 0):
+                return True
+        else:
+            if solve_backtracking_helper(row, col + 1):
+                return True
+
+        # This number did not work continue to the next one
+        board[row][col] = 0
+
+    # If the function reached this point then it has exhausted all possible numbers in this position without success
+    return False
 
 
 # Inputs: None
 # Outputs: valid (Boolean)
 # Returns true if all of the non-zero spaces on the board are valid
 def valid_board():
-
     for row in range(9):
         for col in range(9):
             if board[row][col] != 0:
@@ -105,7 +156,6 @@ def valid_board():
     return True
 
 
-
 board = array([[0, 0, 0, 0, 0, 1, 0, 0, 0],
                [0, 0, 0, 0, 0, 0, 0, 0, 0],
                [0, 0, 0, 0, 0, 6, 0, 0, 0],
@@ -116,4 +166,4 @@ board = array([[0, 0, 0, 0, 0, 1, 0, 0, 0],
                [0, 0, 0, 0, 0, 3, 0, 0, 0],
                [0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
-print(valid_numbers(0, 0))
+print(solve_backtracking())
